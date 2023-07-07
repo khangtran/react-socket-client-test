673312e4d81814b61938af644d12a26cfbc6d5f3
khangtran <tqkhang93@gmail.com>
2023-07-07 10:49:41 +0700

    - update simple ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import './style.css';
 import io from 'socket.io-client';
 
 export default class App extends React.PureComponent {
+
+  componentDidMount() {
+    console.log('event document');
+    if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+
+        }
+      })
+    }
+  }
+
+  componentWillUnmount() {
+
+  }
+
   render() {
     return (
       <div>
@@ -11,6 +28,20 @@ export default class App extends React.PureComponent {
         <input ref={(c) => (this.field = c)} placeholder="url socket" />
 
         <button onClick={() => this.onClick()}>Connect</button>
+        <div style={{ height: 20 }} />
+        <div>
+          <button onClick={() => this.onUserConnect()} >User Connect</button>
+        </div>
+
+
+        <div>
+          <input ref={c => this.field_title = c} placeholder='title' ></input>
+          <input ref={c => this.field_message = c} placeholder='message' ></input>
+          <input ref={c => this.field_group = c} placeholder='group' ></input>
+          <button onClick={() => this.onPushNotification()}>push notification</button>
+        </div>
+
+        <textarea ref={c => this.field_event = c} style={{ height: 300, width: 400 }} />
       </div>
     );
   }
@@ -22,10 +53,62 @@ export default class App extends React.PureComponent {
     this.setupSocketEvent();
   }
 
+  onUserConnect() {
+    var user = {
+      user_id: 1234, name_user: 'user@socket.test', store: 'H001', time_login: Date.now().toLocaleString(), group: ['H001', 'H002']
+    }
+    this.socket.emit('user_connected', user);
+  }
+
+  async onPushNotification() {
+    var url = 'http://localhost:3000/api/v1/pushnotifi'
+    var res = await fetch(url, {
+      method: 'post', body: {
+        title: this.field_title.value,
+        message: this.field_message.value,
+        group: this.field_group.value
+      }
+    });
+
+    if (res.status == 200) {
+      this.makeLogEvent('pushNotification.success')
+    }
+    else if (res.status != 200) { this.makeLogEvent('pushNotification.error') }
+
+  }
+
   setupSocketEvent() {
     console.log(this.socket)
     this.socket.on('connect', () => {
-      console.log(`socket.connectId ${this.socket.id}`);
+      var str = `socket.connectId ${this.socket.id}`
+      this.makeLogEvent(str);
+      console.log(str);
     });
+
+    this.socket.on('disconnect', () => {
+      this.makeLogEvent('disconnected')
+      console.log(`disconnected`);
+    })
+
+    this.socket.on('getalluserclient', (data) => {
+      var user = data[data.length - 1];
+      console.log('getalluserclient', data, user);
+
+      var userLatest = `userLast ${user['user_id']}`
+      this.makeLogEvent(userLatest)
+    })
+
+    this.socket.on('send-notifi', (data) => {
+      var { title, message } = data;
+      this.makeLogEvent(`notification.reveice ${title} ${message}`)
+
+      var notifi = new Notification(title, { body: message });
+    })
   }
+
+
+  makeLogEvent(string) {
+    this.field_event.value += `${string} \n`
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+//debug
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+//release
+root.render(<App />)
